24cf9f3ebc9bd2d3664871980072be75c215a2ba
Pierre Chaussalet <pierre.chaussalet@orange-vallee.net>
2013-04-17 23:34:39 +0200

    Handle missing pem key

--- ssh_keys.sh
+++ ssh_keys.sh
@@ -1,6 +1,6 @@
 
 ssh-add -l |grep 'd8:07:72:5c:50:44:f7:d4:7c:a2:0f:37:59:36:0a:ee' >/dev/null
-if [[ $? != 0 ]]; then
+if [[ $? != 0 && -f .ssh/pchaussalet.pem ]]; then
   ssh-add .ssh/pchaussalet.pem
 fi
 
