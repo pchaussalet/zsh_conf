92f545ce31fffc14695db5e83a6835b7e3b1af83
Pierre Chaussalet <pchaussalet@gmail.com>
2015-07-22 18:37:34 +0200

    Ensure required pacmd command is available

--- aliases.sh
+++ aliases.sh
@@ -15,7 +15,9 @@ alias l='ls -CF'
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-alias pa_mono="pacmd load-module module-remap-sink sink_name=mono master=`pacmd list-sinks | grep name: | grep analog |awk -F'<' '{ print $2 }' |awk -F'>' '{ print $1 }'` channels=2 channel_map=mono,mono"
+if which pacmd > /dev/null; then
+  alias pa_mono="pacmd load-module module-remap-sink sink_name=mono master=`pacmd list-sinks | grep name: | grep analog |awk -F'<' '{ print $2 }' |awk -F'>' '{ print $1 }'` channels=2 channel_map=mono,mono"
+fi
 alias cleanup_id3='for file in *.mp3; do track=`echo $file |awk -F"_" '"'"'{ print $1 }'"'"'`; title=`echo $file |awk -F"_" '"'"'{ print $2 }'"'"' |sed "s/\.mp3$//"`; current=`pwd`; parent=`dirname "$current"`; album=`basename "$current"`; artist=`basename "$parent"`; id3v2 -D $file; id3v2 -T $track -t "$title" -a "$artist" -A "$album" $file ;done'
 
 alias virlxc='virsh -c lxc:///' 
